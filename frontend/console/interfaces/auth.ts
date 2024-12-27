f13dcc77a16b611abc74123f1649969c3723e562
飘丨雪 <2945414535@qq.com>
2024-12-27 17:15:46 +0800

    console:signin and signup

---
+++ frontend/console/interfaces/auth.ts
@@ -0,0 +1,11 @@
+export interface RegisterParams {
+    username?: string;
+    password?: string;
+    email?: string;
+}
+
+export interface LoginParams {
+    email?: string;
+    password?: string;
+}
+
