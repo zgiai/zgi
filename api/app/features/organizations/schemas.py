64860a1d09a187b12d48b45129806e4c61d42b89
blinkmj <511933643@qq.com>
2024-12-29 17:57:37 +0800

    add project with organization create

--- api/app/features/organizations/schemas.py
+++ api/app/features/organizations/schemas.py
@@ -7,8 +7,12 @@ class OrganizationBase(BaseModel):
     name: str = Field(..., min_length=1, max_length=255)
     description: Optional[str] = Field(None, max_length=1000)
 
+class OrgProjectCreateBase(BaseModel):
+    name: str = Field(..., min_length=1, max_length=255)
+    description: Optional[str] = Field(None, max_length=1000)
+
 class OrganizationCreate(OrganizationBase):
-    pass
+    project: Optional[OrgProjectCreateBase] = None
 
 class OrganizationUpdate(BaseModel):
     name: Optional[str] = Field(None, min_length=1, max_length=255)
