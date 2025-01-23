ac505ca9e3067762eb8bd4a2c5175fbccbaa4108
blinkmj <511933643@qq.com>
2025-01-23 22:47:56 +0800

    fix(app): revert CORS allow_origins to origins

--- api/app/main.py
+++ api/app/main.py
@@ -88,8 +88,7 @@ origins = [
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
